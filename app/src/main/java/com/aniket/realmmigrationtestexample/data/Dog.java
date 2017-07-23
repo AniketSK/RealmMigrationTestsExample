67fc7f2876131a574eb1ffc6c10520ef40e22842
Aniket Kadam <anikadamg@gmail.com>
2017-07-23 22:24:58 +0530

    Added a dog class.

---
+++ app/src/main/java/com/aniket/realmmigrationtestexample/data/Dog.java
@@ -0,0 +1,11 @@
+package com.aniket.realmmigrationtestexample.data;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by nicky on 23/7/17.
+ */
+
+public class Dog extends RealmObject {
+    String name;
+}

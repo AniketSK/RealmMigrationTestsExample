fc18d085ee0dd7b060f0c1f8dc5238067543a538
Aniket Kadam <anikadamg@gmail.com>
2017-07-24 00:12:41 +0530

    Operate on the realm instance that's newly created, not the one for the app.

--- app/src/androidTest/java/com/aniket/realmmigrationtestexample/MigrationExampleTest.java
+++ app/src/androidTest/java/com/aniket/realmmigrationtestexample/MigrationExampleTest.java
@@ -35,16 +35,19 @@ public class MigrationExampleTest {
     public void migrate_migrationNeededIsThrown() throws Exception {
         String REALM_NAME = "realmdb_1.realm";
         RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+                .name(REALM_NAME)
                 .schemaVersion(1) // Original realm version.
                 .build();// Get a configuration instance.
         configFactory.copyRealmFromAssets(context, REALM_NAME, realmConfig); // Copy the stored version 1 realm file from assets to a NEW location.
         Realm.getInstance(realmConfig);
     }
 
+
     @Test
     public void migrate_migrationSuceeds() throws Exception {
         String REALM_NAME = "realmdb_1.realm"; // Same name as the file for the old realm which was copied to assets.
         RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+                .name(REALM_NAME)
                 .schemaVersion(2) // NEW realm version.
                 .migration(new MigrationExample())
                 .build();// Get a configuration instance.

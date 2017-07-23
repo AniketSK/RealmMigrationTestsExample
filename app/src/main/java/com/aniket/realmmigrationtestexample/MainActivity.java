5328aa137d3b13a4c41176317e2db4f94bc66881
Aniket Kadam <anikadamg@gmail.com>
2017-07-23 22:16:46 +0530

    Gotten a realm instance.

--- app/src/main/java/com/aniket/realmmigrationtestexample/MainActivity.java
+++ app/src/main/java/com/aniket/realmmigrationtestexample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.aniket.realmmigrationtestexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .schemaVersion(1)
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+        Realm realm = Realm.getDefaultInstance();
+        realm.close();
     }
 }
